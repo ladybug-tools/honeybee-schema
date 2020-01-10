e740e3fa953a6a44d128db7f7c14fcb29ef34a19
Chris <chris@ladybug.tools>
2020-01-09 22:04:57 -0500

    feat(pip): Set up deploment to PyPI
    This officially puts the honeybee_schema package on PyPI so that it can be used by other schema repos (like the future dragonfly_schema repo).

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Building distribution"
-# python setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel
 echo "Pushing new version to PyPi"
-# twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
+twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD

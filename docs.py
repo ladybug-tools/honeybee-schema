43784532828b907f09d9f6a776f60d8307f143c0
Mostapha <sadeghipour@gmail.com>
2020-04-20 09:16:34 -0400

    refactor(openapi): make get_openapi function reusable
    fix #88

--- docs.py
+++ docs.py
@@ -1,4 +1,5 @@
 """generate openapi docs."""
+from pkg_resources import get_distribution
 from honeybee_schema._openapi import get_openapi, get_model_mapper
 from honeybee_schema.model import Model
 from honeybee_schema.energy.simulation import SimulationParameter
@@ -17,6 +18,31 @@ VERSION = None
 
 if args.version:
     VERSION = args.version.replace('v', '')
+else:
+    try:
+        VERSION = '.'.join(get_distribution('honeybee_schema').version.split('.')[:3]),
+    except:
+        pass
+
+info = {
+        "description": "",
+        "version": VERSION,
+        "title": "",
+        "contact": {
+            "name": "Ladybug Tools",
+            "email": "info@ladybug.tools",
+            "url": "https://github.com/ladybug-tools/honeybee-schema"
+        },
+        "x-logo": {
+            "url": "https://www.ladybug.tools/assets/img/honeybee-large.png",
+            "altText": "Honeybee logo"
+        },
+        "license": {
+            "name": "BSD",
+            "url": "https://github.com/ladybug-tools-in2/honeybee-schema/blob/master/LICENSE"
+        }
+}
+
 
 # generate Model open api schema
 print('Generating Model documentation...')
@@ -30,7 +56,7 @@ openapi = get_openapi(
     [Model],
     title='Honeybee Model Schema',
     description='This is the documentation for Honeybee model schema.',
-    version=VERSION,
+    version=VERSION, info=info,
     external_docs=external_docs)
 with open('./docs/model.json', 'w') as out_file:
     json.dump(openapi, out_file, indent=2)
@@ -40,7 +66,7 @@ openapi = get_openapi(
     [Model],
     title='Honeybee Model Schema',
     description='This is the documentation for Honeybee model schema.',
-    version=VERSION,
+    version=VERSION, info=info,
     inheritance=True,
     external_docs=external_docs
 )
@@ -66,7 +92,7 @@ openapi = get_openapi(
     [SimulationParameter],
     title='Honeybee Energy Simulation Parameter Schema',
     description='This is the documentation for Honeybee energy simulation parameter schema.',
-    version=VERSION,
+    version=VERSION, info=info,
     external_docs=external_docs)
 with open('./docs/simulation-parameter.json', 'w') as out_file:
     json.dump(openapi, out_file, indent=2)
@@ -75,7 +101,7 @@ openapi = get_openapi(
     [SimulationParameter],
     title='Honeybee Energy Simulation Parameter Schema',
     description='This is the documentation for Honeybee energy simulation parameter schema.',
-    version=VERSION, inheritance=True,
+    version=VERSION, inheritance=True, info=info,
     external_docs=external_docs
 )
 with open('./docs/simulation-parameter_inheritance.json', 'w') as out_file:

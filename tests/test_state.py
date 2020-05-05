ff7d4a5dfc77c7087a21d1f9f3d1975dd88487dd
Saeran <saeranv@gmail.com>
2020-05-04 22:13:26 -0400

    fix(state): Partial fix for circular imports in states.

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,5 +1,7 @@
 import os
-from honeybee_schema.radiance.state import RadianceSubFaceStateAbridged
+from honeybee_schema.radiance.state import RadianceSubFaceStateAbridged, \
+    RadianceShadeStateAbridged
+
 
 # target folder where all of the samples live
 root = os.path.dirname(os.path.dirname(__file__))
@@ -10,3 +12,7 @@ def test_aperture_state_abridged_bsdf():
     file_path = os.path.join(target_folder, 'aperture_state_abridged_bsdf.json')
     RadianceSubFaceStateAbridged.parse_file(file_path)
 
+
+def test_shade_state_abridged_snow():
+    file_path = os.path.join(target_folder, 'shade_state_abridged_snow.json')
+    RadianceShadeStateAbridged.parse_file(file_path)

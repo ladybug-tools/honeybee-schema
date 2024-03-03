9d9ea58368be0b6d047894e79b1324e49c4f3d6c
Chris Mackey <chris@ladybug.tools>
2024-03-03 11:16:50 -0800

    fix(setpoint): Add an option for setpoint_cutout_difference

--- honeybee_schema/energy/load.py
+++ honeybee_schema/energy/load.py
@@ -472,6 +472,26 @@ class SetpointAbridged(IDdEnergyBaseModel):
         'The values in this schedule should be in [%].'
     )
 
+    setpoint_cutout_difference: float = Field(
+        0,
+        ge=0,
+        description='An optional positive number for the temperature '
+        'difference between the cutout temperature and the setpoint temperature. '
+        'Specifying a non-zero number here is useful for modeling the throttling '
+        'range associated with a given setup of setpoint controls and HVAC equipment. '
+        'Throttling ranges describe the range where a zone is slightly over-cooled '
+        'or over-heated beyond the thermostat setpoint. They are used to avoid '
+        'situations where HVAC systems turn on only to turn off a few minutes later, '
+        'thereby wearing out the parts of mechanical systems faster. They can '
+        'have a minor impact on energy consumption and can often have significant '
+        'impacts on occupant thermal comfort, though using the default value '
+        'of zero will often yield results that are close enough when trying '
+        'to estimate the annual heating/cooling energy use. Specifying a value '
+        'of zero effectively assumes that the system will turn on whenever '
+        'conditions are outside the setpoint range and will cut out as soon '
+        'as the setpoint is reached.'
+    )
+
     @root_validator
     def check_both_humid_sch(cls, values):
         "Ensure that the other humidity schedule is included when one is."
